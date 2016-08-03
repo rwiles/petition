56c7c5732e0f19ab8a815b24e88030969917de90
rwiles <robert@rwiles.com>
2016-08-03 18:47:20 -0400

    Set up initial migrations and views

---
+++ database/migrations/2016_08_03_223455_create_petition_tables.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetitionTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('petitions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('summary');
+            $table->string('body');
+            $table->string('thankyou_title');
+            $table->string('thankyou_body');
+            $table->string('thankyou_email_subject');
+            $table->string('thankyou_email_body');
+            $table->string('thankyou_sms');
+            $table->timestamps();
+        });
+
+        Schema::create('signatures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('petition_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('petitions');
+        Schema::drop('signatures');
+    }
+}
